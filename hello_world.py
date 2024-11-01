1d914da3c0f789bfc650f615a9858d4f701de16d
William Chau <wchau.misc@gmail.com>
2024-11-01 13:19:12 +0100

    fixed bug in hello_world.py, removed comma from filename

--- hello_world.py
+++ hello_world.py
@@ -2,7 +2,8 @@ from datetime import date
 from functions import main
 # dit is mijn eerste Python programma
 
-
+# import sys 
+# print (sys.path)
 main()
 
 vandaag = date.today()
---
+++ user_input.py
@@ -0,0 +1,6 @@
+def main():
+    today = input('wat voor dag is het vandaag? ')
+    print (' wow, vandaag is', today)
+
+
+main()
